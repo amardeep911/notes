4779393d50a417433bd10e88434ca5569c16610f
Kuldeep Ranjan <ranjankuldeep19@gmail.com>
2022-09-07 16:21:55 +0530

    deleted some unused file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 // import Card from './Components/Card/Card'
-import Home from './Components/Home/Home'
+
 import Ui from './Components/Home/Ui'
-import Modal from './Components/Modal/Modal'
+
 const App = (props) => {
   return (
     <div>
       <Ui/>
-      
     </div>
   )
 }
